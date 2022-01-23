660b02f4fc86500b352610207a7d903826e6754a
Piotr <pitbroo@gmail.com>
2022-01-23 12:54:10 +0100

    add Currency Controller

---
+++ src/main/java/pl/piotrbrodziak/zadaniepolitechnika/controller/CurrencyController.java
@@ -0,0 +1,36 @@
+package pl.piotrbrodziak.zadaniepolitechnika.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.piotrbrodziak.zadaniepolitechnika.HttpClient.CurrencyClient;
+import pl.piotrbrodziak.zadaniepolitechnika.dto.CurrencyDto;
+import pl.piotrbrodziak.zadaniepolitechnika.dto.Rates;
+import pl.piotrbrodziak.zadaniepolitechnika.service.CurrencyService;
+import pl.piotrbrodziak.zadaniepolitechnika.service.RatesService;
+
+@RestController
+@RequestMapping("rates")
+public class CurrencyController {
+
+    private final CurrencyClient currencyClient;
+    private final RatesService ratesService;
+    private final CurrencyService currencyService;
+
+    public CurrencyController(CurrencyClient currencyClient, RatesService ratesService, CurrencyService currencyService) {
+        this.currencyClient = currencyClient;
+        this.ratesService = ratesService;
+        this.currencyService = currencyService;
+    }
+
+    @GetMapping("{currency}/{date1}/{date2}")
+    public CurrencyDto getCurrency(@PathVariable String currency, @PathVariable String date1,@PathVariable String date2 ){
+        Rates rates1 = currencyClient.getCurrencyOneFromURL(currency, date1);
+        Rates rates2 = currencyClient.getCurrencyOneFromURL(currency, date2);
+        CurrencyDto currencyDto = currencyService.designateCurrencyDto(rates1, rates2);
+        return currencyDto;
+    }
+
+
+}
