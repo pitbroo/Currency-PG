af1ffcf6f739f60b900dfeef9656f63c8a546a26
Piotr <pitbroo@gmail.com>
2022-01-23 12:37:01 +0100

    Initial Sptring Boot App

---
+++ src/main/java/pl/piotrbrodziak/zadaniepolitechnika/ZadaniePolitechnikaApplication.java
@@ -0,0 +1,15 @@
+package pl.piotrbrodziak.zadaniepolitechnika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadaniePolitechnikaApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ZadaniePolitechnikaApplication.class, args);
+
+    }
+
+}
